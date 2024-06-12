cd0d49f597c28000e30cbc7abfe214a7fad2a00e
Narek Iskandaryan Arturovich <xxxx30411@gmail.com>
2024-06-12 16:48:51 +0300

    send

---
+++ source/repos/10.06.2024/10.06.2024/Class.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal {
+private:
+	int x = 0;
+	int y = 0;
+
+public:
+	void move(int new_x, int new_y) {
+		x = new_x;
+		y = new_y;
+	}
+
+	void get_pos() {
+		cout << x << ", " << y << endl;
+	}
+
+	void sound() {
+		cout << "RRRRR" << endl;
+	}
+};
+
+class Cat :public Animal {
+public:
+	void sound() {
+		cout << "Myau Myau" << endl;
+	}
+};
+
+class Dog :public Animal {
+public:
+	void sound() {
+		cout << "Gav Gav" << endl;
+	}
+};
+
+void test_animal() {
+	Dog dog;
+	Cat cat;
+	dog.sound();
+	cat.sound();
+}
+
+
+
+int main() {
+	system("chcp 1251");
+	system("cls");
+
+
+	test_animal();
+	
+;
+
+	return 0;
+}
